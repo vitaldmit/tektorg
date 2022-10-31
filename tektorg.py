f7b80606dcf7ff64428535ba3329cc87aa7f5eb2
Виталий Дмитриев <vitaldmit@bk.ru>
2022-10-31 21:37:48 +0300

    if added

--- tektorg.py
+++ tektorg.py
@@ -3,22 +3,23 @@ from bs4 import BeautifulSoup
 from secrets import TOKEN, CHATID
 
 
-URL = 'https://www.tektorg.ru/market/procedures?q=Канаш&region=Чувашская+Республика&status=270&lang=ru&sort=datestart&limit=500'
+URL = 'https://www.tektorg.ru/market/procedures?q=Янтиков&region=Чувашская+Республика&status=270&lang=ru&sort=datestart&limit=500'
 page = requests.get(URL)
 soup = BeautifulSoup(page.text, 'html.parser')
 
 LINKS = soup.find_all('a', class_='section-procurement__item-title')
 
-with open('last.txt') as f:
-    last_title = str(f.readline())
+if LINKS:
+    with open('last.txt') as f:
+        last_title = str(f.readline())
 
-for link in LINKS:
-    if link.text != last_title:
-        # print(link.text)
-        requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&parse_mode=html&text=%s' % (TOKEN, CHATID, link.text))
-        time.sleep(1)
-    else:
-        with open('last.txt', 'w') as f:
-            f.write(str(LINKS[0].text))
-        break
+    for link in LINKS:
+        if link.text != last_title:
+            # print(link.text)
+            requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&parse_mode=html&text=%s' % (TOKEN, CHATID, link.text))
+            time.sleep(1)
+        else:
+            with open('last.txt', 'w') as f:
+                f.write(str(LINKS[0].text))
+            break
 
