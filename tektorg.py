89e71f249abac31265083647e10f94305c89b2f1
Виталий Дмитриев <vitaldmit@bk.ru>
2022-11-01 11:08:35 +0300

    Основной шаблон готов. Осталось сделать улучшение

--- tektorg.py
+++ tektorg.py
@@ -1,9 +1,10 @@
+import time
 import requests
 from bs4 import BeautifulSoup
 from secrets import TOKEN, CHATID
 
 
-URL = 'https://www.tektorg.ru/market/procedures?q=Янтиков&region=Чувашская+Республика&status=270&lang=ru&sort=datestart&limit=500'
+URL = 'https://www.tektorg.ru/market/procedures?region=Чувашская+Республика&status=270&lang=ru&sort=datestart&limit=500'
 page = requests.get(URL)
 soup = BeautifulSoup(page.text, 'html.parser')
 
@@ -11,10 +12,10 @@ LINKS = soup.find_all('a', class_='section-procurement__item-title')
 
 if LINKS:
     with open('last.txt') as f:
-        last_title = str(f.readline())
+        last_title = str(f.readline()).strip()
 
     for link in LINKS:
-        if link.text != last_title:
+        if link.text.strip() != last_title:
             # print(link.text)
             requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&parse_mode=html&text=%s' % (TOKEN, CHATID, link.text))
             if len(LINKS) > 1:
@@ -22,5 +23,5 @@ if LINKS:
         else:
             break
 
-with open('last.txt', 'w') as f:
-    f.write(str(LINKS[0].text))
+    with open('last.txt', 'w') as f:
+        f.write(str(LINKS[0].text.strip()))
