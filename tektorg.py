942645df88a21311411f280a7f5224e3d299641e
Виталий Дмитриев <vitaldmit@bk.ru>
2022-11-02 18:03:50 +0300

    Поменял на Янтиков

--- tektorg.py
+++ tektorg.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 from secrets import TOKEN, CHATID
 
 
-URL = 'https://www.tektorg.ru/market/procedures?status=270&region=%D0%A7%D1%83%D0%B2%D0%B0%D1%88%D1%81%D0%BA%D0%B0%D1%8F+%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0&lang=ru&sort=datestart&order=desc&limit=500'
-# URL = 'https://www.tektorg.ru/market/procedures?q=%D0%AF%D0%BD%D1%82%D0%B8%D0%BA%D0%BE%D0%B2&region=%D0%A7%D1%83%D0%B2%D0%B0%D1%88%D1%81%D0%BA%D0%B0%D1%8F+%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0&status=270&lang=ru&sort=datestart&limit=500'
+URL = 'https://www.tektorg.ru/market/procedures?q=%D0%AF%D0%BD%D1%82%D0%B8%D0%BA%D0%BE%D0%B2&region=%D0%A7%D1%83%D0%B2%D0%B0%D1%88%D1%81%D0%BA%D0%B0%D1%8F+%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0&status=270&lang=ru&sort=datestart&limit=500'
 page = requests.get(URL)
 soup = BeautifulSoup(page.text, 'html.parser')
 items = []
