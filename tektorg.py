d12f4a76c266f733ba91884201f8e282294f80c1
Виталий Дмитриев <vitaldmit@bk.ru>
2022-11-24 14:13:52 +0300

    Добавил цену

--- tektorg.py
+++ tektorg.py
@@ -1,32 +1,32 @@
-import time
 import requests
 from bs4 import BeautifulSoup
+
 from secrets import TOKEN, CHATID
 
 
 URL = 'https://www.tektorg.ru/market/procedures?q=%D0%AF%D0%BD%D1%82%D0%B8%D0%BA%D0%BE%D0%B2&region=%D0%A7%D1%83%D0%B2%D0%B0%D1%88%D1%81%D0%BA%D0%B0%D1%8F+%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0&status=270&lang=ru&sort=datestart&limit=500'
+
 page = requests.get(URL)
 soup = BeautifulSoup(page.text, 'html.parser')
-items = []
+messages = []
 
-LINKS = soup.find_all('a', class_='section-procurement__item-title')
+ITEMS = soup.find_all('div', {'class': 'section-procurement__item-info'})
 
-if LINKS:
-    with open('last.txt', 'r') as f:
+if ITEMS:
+    with open('last.txt', 'r', encoding='utf-8') as f:
         last = str(f.readline()).strip()
 
-    i = 1
-    for link in LINKS:
+    for item in ITEMS:
+        link = item.find('a', {'class': 'section-procurement__item-title'})
+        price = item.find('div', {'class': 'section-procurement__item-totalPrice'})
         if link.text.strip() != last:
-            items.append(f"{i}) {link.text}: https://www.tektorg.ru{link['href']}")
-            i+=1
+            messages.append(f"- [{link.text}](https://www.tektorg.ru{link['href']}) *цена {price.text}*")
         else:
             break
 
-    to_telegram = '\n\n'.join(str(v) for v in items)
-    requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&parse_mode=html&text=%s' % (TOKEN, CHATID, to_telegram))
-    # print(to_telegram)
+    to_telegram = '\n\n'.join(str(m) for m in messages)
+    response = requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&parse_mode=Markdown&text=%s' % (TOKEN, CHATID, to_telegram))
 
-    with open('last.txt', 'w') as f:
-        f.write(str(LINKS[0].text.strip()))
+    with open('last.txt', 'w', encoding='utf-8') as f:
+        f.write(str(link.text.strip()))
 
